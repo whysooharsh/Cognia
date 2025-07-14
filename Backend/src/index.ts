45c6158757fe4b698f17b14934e12abac0bfccb8
Harsh Sharma <harsh.sharma.8986@gmail.com>
2025-07-14 20:58:52 +0530

    feat: update delete endpoint

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -156,8 +156,19 @@ app.get("/api/v1/content", userMiddleware, async (req, res) => {
 })
 
 
-app.delete("/api/v1/content", (req, res) => {
+app.delete("/api/v1/content", async (req, res) => {
 
+    const contentId = req.body.contentId;
+
+    await contentModel.deleteMany({
+        contentId, 
+        // @ts-ignore
+        userId : req.userId, 
+    })
+
+    res.json({
+        message : "Deleted"
+    })
 })
 
 app.post("/api/v1/brain/share", (req, res) => {
